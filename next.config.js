b1cf9bfa9018926d8df70a06b9ca9dbbf6e88634
David Varela <de-varela@outlook.com>
2023-02-13 05:59:52 +0000

    fix: try using custom build directory for cloudflare

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const withMarkdoc = require('@markdoc/next.js');
 
 module.exports = withMarkdoc({ schemaPath: './src/markdoc' })({
+  distDir: 'build',
   pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx'],
   sassOptions: {
     includePaths: [path.join(__dirname, 'node_modules')],
