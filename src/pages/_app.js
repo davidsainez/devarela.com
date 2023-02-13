8e1b886e6feea71eca04d70300c05117d514a196
David Varela <de-varela@outlook.com>
2023-02-13 07:25:46 +0000

    feat: include metadata to improve lighthouse scores

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../scss/index.scss';
 import { useRouter } from 'next/router';
 import { Layout } from '../components/Layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -8,6 +9,13 @@ export default function App({ Component, pageProps }) {
 
   return (
     <Layout path={path} markdoc={pageProps.markdoc}>
+      <Head>
+        <title>D.E. Varela</title>
+        <meta
+          name="description"
+          content="D.E. Varela's collection of essays on web technology, design, software principles, and entrepreneurship."
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
