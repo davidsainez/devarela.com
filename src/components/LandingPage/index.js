464545715d06881bb914bac4215c88a3b502dd19
David Varela <00.varela.david@gmail.com>
2023-02-16 20:27:43 +0000

    chore: remove unnecessary comment

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -4,54 +4,6 @@ import { Theme, ClickableTile, Grid, Column } from '@carbon/react';
 import { ArrowRight } from '@carbon/icons-react';
 import { Typewriter } from '../Typewriter';
 
-/*
-const SocialLink = ({ href, text, children }) => {
-  return (
-    <a href={href} className="landing__link cds--link">
-      <span className="landing__link__icon">{children}</span>
-      {text}
-    </a>
-  );
-};
-
-const LandingPageCard = () => {
-  return (
-      <div className="landing__card">
-        <div className="landing__heading">
-          <Typewriter />
-          <p className="landing__quote cds--type-serif">
-            What I cannot create, I do not understand
-          </p>
-        </div>
-        <div className="landing__meta">
-          <Stack className="landing__social">
-            <SocialLink
-              text="@devarela_com"
-              href="https://twitter.com/devarela_com"
-            >
-              <LogoTwitter />
-            </SocialLink>
-            <SocialLink text="@de-varela" href="https://github.com/de-varela">
-              <LogoGithub />
-            </SocialLink>
-            <SocialLink
-              text="de-varela@outlook.com"
-              href="mailto:de-varela@outlook.com"
-            >
-              <Email />
-            </SocialLink>
-          </Stack>
-          <div className="landing__action">
-            <Link href="/essays" icon={ArrowRight}>
-              Essays
-            </Link>
-          </div>
-        </div>
-      </div>
-  );
-};
-*/
-
 const TITLE = 'D.E. Varela';
 const URL = 'devarela.com';
 const DESCRIPTION = `
