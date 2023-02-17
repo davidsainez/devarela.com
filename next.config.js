752de2d086667ebde04f57f84726fb242a9e3386
David Varela <00.varela.david@gmail.com>
2023-02-17 01:18:56 +0000

    feat: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,14 +3,20 @@
 const path = require('path');
 const withMarkdoc = require('@markdoc/next.js');
 
-module.exports = withMarkdoc({ schemaPath: './src/markdoc' })({
-  distDir: 'build',
-  pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx'],
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'node_modules')],
-  },
-  webpack(config) {
-    config.resolve.alias['~'] = path.join(__dirname, 'src');
-    return config;
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
+
+module.exports = withBundleAnalyzer(
+  withMarkdoc({ schemaPath: './src/markdoc' })({
+    distDir: 'build',
+    pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx'],
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'node_modules')],
+    },
+    webpack(config) {
+      config.resolve.alias['~'] = path.join(__dirname, 'src');
+      return config;
+    },
+  })
+);
