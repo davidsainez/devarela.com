4e024a2b5c849b95a16fb01967b75ef9b773455a
David Varela <00.varela.david@gmail.com>
2023-03-03 19:52:02 +0000

    chore: some more visual tweaks

--- src/components/EssayIndex/Card/index.js
+++ src/components/EssayIndex/Card/index.js
@@ -1,18 +1,14 @@
 import * as NextLink from 'next/link';
-import { RiArrowRightLine } from 'react-icons/ri';
 import styles from './index.module.scss';
 
 export const Card = ({ href, title, date, summary }) => {
   return (
-    <div className={styles.box}>
+    <NextLink href={href} className={styles.box}>
       <span className={styles.date}>{date}</span>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.summary}>
         <p>{summary}</p>
       </div>
-      <NextLink href={href} className={styles.read}>
-        Read <RiArrowRightLine />
-      </NextLink>
-    </div>
+    </NextLink>
   );
 };
