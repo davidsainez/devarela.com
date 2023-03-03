685ac9ded8b95cac8a66cb737752c06b11b53006
David Varela <00.varela.david@gmail.com>
2023-03-03 20:28:10 +0000

    feat: adjust styles for essay tags

--- src/components/EssayIndex/Card/index.js
+++ src/components/EssayIndex/Card/index.js
@@ -1,9 +1,16 @@
 import * as NextLink from 'next/link';
 import styles from './index.module.scss';
 
-export const Card = ({ href, title, date, summary }) => {
+export const Card = ({ href, title, date, summary, tags }) => {
   return (
     <NextLink href={href} className={styles.box}>
+      <div className={styles.tags}>
+        {tags.map((tag) => (
+          <span key={tag} className={styles.tag}>
+            {tag}
+          </span>
+        ))}
+      </div>
       <span className={styles.date}>{date}</span>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.summary}>
