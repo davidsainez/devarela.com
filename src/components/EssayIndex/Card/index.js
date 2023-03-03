4606309f2c27698bf70d9d191562cdb8c3216a8a
David Varela <00.varela.david@gmail.com>
2023-03-03 21:03:11 +0000

    fix: tighter content view on mobile

--- src/components/EssayIndex/Card/index.js
+++ src/components/EssayIndex/Card/index.js
@@ -4,17 +4,15 @@ import styles from './index.module.scss';
 export const Card = ({ href, title, date, summary, tags }) => {
   return (
     <NextLink href={href} className={styles.box}>
+      <span className={styles.date}>{date}</span>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.summary}>{summary}</p>
       <div className={styles.tags}>
         {tags.map((tag) => (
           <span key={tag} className={styles.tag}>
             {tag}
           </span>
         ))}
-      </div>
-      <span className={styles.date}>{date}</span>
-      <h2 className={styles.title}>{title}</h2>
-      <div className={styles.summary}>
-        <p>{summary}</p>
       </div>
     </NextLink>
   );
