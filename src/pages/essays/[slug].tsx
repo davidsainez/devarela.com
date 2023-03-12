8f0398ee7b784c591ee3ba7237923df8d47e6b56
David Varela <00.varela.david@gmail.com>
2023-03-12 13:51:35 +0000

    fix: add typings for markdoc renderer

--- src/pages/essays/[slug].tsx
+++ src/pages/essays/[slug].tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type { createElement, ComponentType, Fragment, ReactNode } from 'react';
 import glob from 'glob-promise';
 import path from 'path';
 import Markdoc from '@markdoc/markdoc';
@@ -8,11 +9,45 @@ import { Paragraph, Heading } from 'components/Article';
 import { Essay } from 'components/Essay';
 import { Code } from 'components/Code';
 
-const isTag = (tag) => {
-  return tag.$$mdtype === 'Tag';
-};
+class Tag<
+  N extends string = string,
+  A extends Record<string, any> = Record<string, any>
+> {
+  readonly $$mdtype = 'Tag' as const;
 
-function tagName(name, components) {
+  static isTag = (tag: any): tag is Tag => {
+    return !!(tag?.$$mdtype === 'Tag');
+  };
+
+  name: N;
+  attributes: A;
+  children: RenderableTreeNode[];
+
+  constructor(
+    name = 'div' as N,
+    attributes = {} as A,
+    children: RenderableTreeNode[] = []
+  ) {
+    this.name = name;
+    this.attributes = attributes;
+    this.children = children;
+  }
+}
+
+type Primitive = null | boolean | number | string;
+type RenderableTreeNode = Tag | Scalar;
+type RenderableTreeNodes = RenderableTreeNode | RenderableTreeNode[];
+type Scalar = Primitive | Scalar[] | { [key: string]: Scalar };
+type Component = ComponentType<unknown>;
+type ReactShape = Readonly<{
+  createElement: typeof createElement;
+  Fragment: typeof Fragment;
+}>;
+
+function tagName(
+  name: string,
+  components: Record<string, Component> | ((string: string) => Component)
+): string | Component {
   return typeof name !== 'string'
     ? name // This can be an object, e.g. when React.forwardRef is used
     : name[0] !== name[0].toUpperCase()
@@ -22,37 +57,31 @@ function tagName(name, components) {
     : components[name];
 }
 
-function renderMarkdoc(node, React, components) {
-  function deepRender(value) {
-    if (value == null || typeof value !== 'object') {
-      return value;
-    }
+function dynamic(
+  node: RenderableTreeNodes,
+  React: ReactShape,
+  { components = {} } = {}
+) {
+  function deepRender(value: any): any {
+    if (value == null || typeof value !== 'object') return value;
 
-    if (Array.isArray(value)) {
-      return value.map((item) => deepRender(item));
-    }
+    if (Array.isArray(value)) return value.map((item) => deepRender(item));
 
-    if (value.$$mdtype === 'Tag') {
-      return render(value);
-    }
+    if (value.$$mdtype === 'Tag') return render(value);
 
-    if (typeof value !== 'object') {
-      return value;
-    }
+    if (typeof value !== 'object') return value;
 
-    const output = {};
-    for (const [k, v] of Object.entries(value)) {
-      output[k] = deepRender(v);
-    }
+    const output: Record<string, Scalar> = {};
+    for (const [k, v] of Object.entries(value)) output[k] = deepRender(v);
     return output;
   }
 
-  function render(node) {
+  function render(node: RenderableTreeNodes) {
     if (Array.isArray(node)) {
       return React.createElement(React.Fragment, null, ...node.map(render));
     }
 
-    if (node === null || typeof node !== 'object' || !isTag(node)) {
+    if (node === null || typeof node !== 'object' || !Tag.isTag(node)) {
       return node;
     }
 
@@ -62,9 +91,7 @@ function renderMarkdoc(node, React, components) {
       children = [],
     } = node;
 
-    if (className) {
-      attrs.className = className;
-    }
+    if (className) attrs.className = className;
 
     return React.createElement(
       tagName(name, components),
@@ -89,7 +116,7 @@ const BlogPostPage = (props) => {
 
   return (
     <Essay metadata={metadata}>
-      {renderMarkdoc(parsedContent, React, components)}
+      {dynamic(parsedContent, React, { components })}
     </Essay>
   );
 };
